876aa325335427a5e32a6127d456280980be1e44
Dragon44PL <krystian.pieniak44@gmail.com>
2021-08-24 17:23:26 +0200

    Changed "totalWorkHour" to "totalWorkHours"

--- domain/src/main/java/com/github/schedule/workmonth/event/TotalHoursCalculatedEvent.java
+++ domain/src/main/java/com/github/schedule/workmonth/event/TotalHoursCalculatedEvent.java
@@ -5,7 +5,7 @@ import com.github.schedule.workmonth.vo.WorkHour;
 import java.time.Instant;
 import java.util.UUID;
 
-public record TotalHoursCalculatedEvent(Instant occurredOn, UUID aggregateId, WorkHour totalWorkHour) implements WorkMonthEvent {
+public record TotalHoursCalculatedEvent(Instant occurredOn, UUID aggregateId, WorkHour totalWorkHours) implements WorkMonthEvent {
 
     public TotalHoursCalculatedEvent(UUID workMonthId, WorkHour totalWorkHour) {
         this(Instant.now(), workMonthId, totalWorkHour);
