c028669d3871cdd18cb8e24e26a156a4ca8bb128
Dragon44PL <krystian.pieniak44@gmail.com>
2021-08-29 12:14:49 +0200

    Changed signature of "findByUserId(UserId)" method to "findByUser(UserId, YearMonth)"

--- workmonth-domain/src/main/java/com/github/schedule/workmonth/WorkMonthRepository.java
+++ workmonth-domain/src/main/java/com/github/schedule/workmonth/WorkMonthRepository.java
@@ -3,9 +3,10 @@ package com.github.schedule.workmonth;
 import com.github.schedule.core.DomainRepository;
 import com.github.schedule.workmonth.vo.UserId;
 
+import java.time.YearMonth;
 import java.util.Optional;
 import java.util.UUID;
 
 interface WorkMonthRepository extends DomainRepository<UUID, WorkMonth> {
-    Optional<WorkMonth> findByUserId(UserId userId);
+    Optional<WorkMonth> findByUser(UserId userId, YearMonth yearMonth);
 }
