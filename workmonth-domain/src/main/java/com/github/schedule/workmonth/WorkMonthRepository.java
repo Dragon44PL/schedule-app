ca79dc6e303c1a4389663084d2a6a877582e18b8
Dragon44PL <krystian.pieniak44@gmail.com>
2021-08-29 12:08:07 +0200

    Added "findByUserId" method

--- workmonth-domain/src/main/java/com/github/schedule/workmonth/WorkMonthRepository.java
+++ workmonth-domain/src/main/java/com/github/schedule/workmonth/WorkMonthRepository.java
@@ -1,7 +1,11 @@
 package com.github.schedule.workmonth;
 
 import com.github.schedule.core.DomainRepository;
+import com.github.schedule.workmonth.vo.UserId;
 
+import java.util.Optional;
 import java.util.UUID;
 
-interface WorkMonthRepository extends DomainRepository<UUID, WorkMonth> { }
+interface WorkMonthRepository extends DomainRepository<UUID, WorkMonth> {
+    Optional<WorkMonth> findByUserId(UserId userId);
+}
