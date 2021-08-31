270543de29239e753459f6d665a20045a371fc52
Dragon44PL <krystian.pieniak44@gmail.com>
2021-08-31 17:34:14 +0200

    Renamed "WorkDayInvalidException" to "WorkDayOutOfRangeException"

---
+++ workmonth-service/domain/src/main/java/com/github/schedule/workmonth/exception/WorkDayOutOfRangeException.java
@@ -0,0 +1,13 @@
+package com.github.schedule.workmonth.exception;
+
+public class WorkDayOutOfRangeException extends RuntimeException {
+
+    public WorkDayOutOfRangeException() {
+        super();
+    }
+
+    public WorkDayOutOfRangeException(String message) {
+        super(message);
+    }
+
+}
