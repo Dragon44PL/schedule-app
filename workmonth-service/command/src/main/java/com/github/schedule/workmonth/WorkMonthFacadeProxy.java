b96428ee2eff331a3379ebc2601203a2527ccfb9
Dragon44PL <krystian.pieniak44@gmail.com>
2021-09-04 15:53:30 +0200

    Created "command" module, closes #8

---
+++ workmonth-service/command/src/main/java/com/github/schedule/workmonth/WorkMonthFacadeProxy.java
@@ -0,0 +1,29 @@
+package com.github.schedule.workmonth;
+
+import com.github.schedule.workmonth.command.WorkDaysChangeCommand;
+import com.github.schedule.workmonth.command.WorkMonthCreateCommand;
+import com.github.schedule.workmonth.event.WorkMonthEvent;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class WorkMonthFacadeProxy extends WorkMonthFacade {
+
+    public WorkMonthFacadeProxy(WorkMonthRepository workMonthRepository) {
+        super(workMonthRepository);
+    }
+
+    @Override
+    @Transactional
+    public List<WorkMonthEvent> createWorkMonth(WorkMonthCreateCommand workMonthCreateCommand) {
+        return super.createWorkMonth(workMonthCreateCommand);
+    }
+
+    @Override
+    @Transactional
+    public List<WorkMonthEvent> updateWorkDays(WorkDaysChangeCommand workDaysChangeCommand) {
+        return super.updateWorkDays(workDaysChangeCommand);
+    }
+}
