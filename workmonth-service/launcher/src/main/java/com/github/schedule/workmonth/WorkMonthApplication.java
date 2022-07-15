3653fac896209bd7b9d0f29deb5f9c1f605cda1e
Dragon44PL <krystian.pieniak44@gmail.com>
2022-07-15 21:15:11 +0200

    enabled service as eureka client

--- workmonth-service/launcher/src/main/java/com/github/schedule/workmonth/WorkMonthApplication.java
+++ workmonth-service/launcher/src/main/java/com/github/schedule/workmonth/WorkMonthApplication.java
@@ -2,7 +2,9 @@ package com.github.schedule.workmonth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class WorkMonthApplication {
     public static void main(String[] args) {
