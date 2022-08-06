376e7331ff2180b8a6235aa89c11d2fac9d5a189
Dragon44PL <krystian.pieniak44@gmail.com>
2022-08-06 17:49:01 +0200

    removed unused imports

--- workmonth-service/adapter/endpoint/src/main/java/com/github/schedule/workmonth/dto/request/workday/WorkDaysChangeDto.java
+++ workmonth-service/adapter/endpoint/src/main/java/com/github/schedule/workmonth/dto/request/workday/WorkDaysChangeDto.java
@@ -1,6 +1,5 @@
 package com.github.schedule.workmonth.dto.request.workday;
 
-import com.github.schedule.workmonth.validation.constraint.UniqueIdentifier;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
