69f544d5144af812b0103617048d8e538662c3a0
Dragon44PL <krystian.pieniak44@gmail.com>
2022-08-12 21:51:32 +0200

    changed @Component to simple class

--- workmonth-service/adapter/command/src/main/java/com/github/schedule/workmonth/WorkMonthEntityConverter.java
+++ workmonth-service/adapter/command/src/main/java/com/github/schedule/workmonth/WorkMonthEntityConverter.java
@@ -3,20 +3,18 @@ package com.github.schedule.workmonth;
 import com.github.schedule.workmonth.vo.UserId;
 import com.github.schedule.workmonth.vo.WorkDay;
 import com.github.schedule.workmonth.vo.WorkHour;
-import org.springframework.stereotype.Component;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Component
 class WorkMonthEntityConverter {
 
-    WorkMonth convertWorkMonth(WorkMonthEntity workMonthEntity) {
-        final Set<WorkDay> workDays = workMonthEntity.getWorkDays().stream().map(this::convertWorkDay).collect(Collectors.toSet());
+    static WorkMonth convertWorkMonth(WorkMonthEntity workMonthEntity) {
+        final Set<WorkDay> workDays = workMonthEntity.getWorkDays().stream().map(WorkMonthEntityConverter::convertWorkDay).collect(Collectors.toSet());
         return WorkMonth.restore(workMonthEntity.getId(), new UserId(workMonthEntity.getUserId()), workMonthEntity.getDate(), workDays);
     }
 
-    WorkDay convertWorkDay(WorkDayEntity workDayEntity) {
+    static WorkDay convertWorkDay(WorkDayEntity workDayEntity) {
         final WorkHour startingHour = new WorkHour(workDayEntity.getStartingHour().getHour(), workDayEntity.getStartingHour().getMinute());
         final WorkHour endingHour = new WorkHour(workDayEntity.getEndingHour().getHour(), workDayEntity.getEndingHour().getMinute());
         return new WorkDay(workDayEntity.getDate(), startingHour, endingHour, workDayEntity.isLeave());
